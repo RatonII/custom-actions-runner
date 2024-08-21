a838b2e33a47b4e0acff025aa361a4081c20965a
Marius Stanescu <marius.s@sliide.com>
2024-08-21 11:18:13 +0300

    Testing keda autoscaler

--- default-runner/entrypoint.sh
+++ default-runner/entrypoint.sh
@@ -2,5 +2,5 @@
 
 export RUNNER_TOKEN=$(curl -L -X POST -H "Accept: application/vnd.github+json" \
      -H "Authorization: Bearer $ACCESS_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" \
-     https://api.github.com/orgs/sliide/actions/runners/registration-token | jq .token)
+     https://api.github.com/orgs/sliide/actions/runners/registration-token | jq -r .token)
 /runner/config.sh --unattended --url $ORG_URL --token $RUNNER_TOKEN --labels $LABELS --ephemeral $EPHEMERAL && source /runner/run.sh
